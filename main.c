73c0862e6a8f5605e1d3fdac8a52a51a48128a99
Felipe Ferraz <felipeferraz2402@gmail.com>
2022-10-28 10:46:05 -0300

    corrigindo o tamanho do tabuleiro

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main()
 {
     //definindo tamanho das linhas e colunas.
-    int linhas = (8*2)+2, colunas = (8*2)+2;
+    int linhas = (9*2)+2, colunas = (9*2)+2;
     //criando o tabuleiro.
     char tabuleiro[linhas][colunas];//criando matriz tabuleiro
     tabuleiro_inicial(linhas, colunas, tabuleiro);//preenchendo as configurações padrões do tabuleiro
